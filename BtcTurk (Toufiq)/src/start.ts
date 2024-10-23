1922d092da591689bf261124860d768b553cb0b3
Toufiqur Chowdhury <htr.letun@gmail.com>
2024-10-23 10:29:44 +0300

    Updated start.ts to create & delete order for demo purposes

--- BtcTurk (Toufiq)/src/start.ts
+++ BtcTurk (Toufiq)/src/start.ts
@@ -3,18 +3,47 @@ import BtcTurkSpotPublicConnector from './connetors/public/btcturk-spot-public-c
 
 const publicKey = process.env.BTCTURK_PUBLIC_KEY || ''
 const privateKey = process.env.BTCTURK_PRIVATE_KEY || ''
+const gotCreds = !!publicKey && !!privateKey
 
 const publicConnector = new BtcTurkSpotPublicConnector(
     { name: 'BTC' },
-    { quoteAsset: 'TRY' },
+    { quoteAsset: 'USDT' },
 )
+
 publicConnector.debug = false
 publicConnector.connect(data => console.log('BtcTurk Public: message received =>', data))
 
-// const privateConnector = new BtcTurkSpotPrivateConnector(
-//     { name: 'BTC' },
-//     { quoteAsset: 'USDT' },
-//     { key: publicKey, secret: privateKey })
+if (gotCreds) {
+    const debugTag = 'BtcTurk Private:'
+    const privateConnector = new BtcTurkSpotPrivateConnector(
+        { name: 'DOT' },
+        { quoteAsset: 'USDT' },
+        { key: publicKey, secret: privateKey })
 
-// privateConnector.debug = false
-// privateConnector.connect(data => console.log('BtcTurk Private: message received =>', data))
+    privateConnector.debug = false
+    console.log('Connecting to private connector')
+    try {
+        privateConnector
+            .connect(data => console.log(debugTag, 'message received =>', data))
+            .then(async () => {
+                console.log(debugTag, 'Websocket Connected.')
+                const order = {
+                    price: 9.99,
+                    side: 'Sell' as 'Sell',
+                    size: 1,
+                    type: 'Limit' as 'Limit',
+                }
+
+                console.log(debugTag, 'placing an order', { ...order })
+
+                const orderResult = await privateConnector.placeOrders({ orders: [order] })
+                console.log(debugTag, 'orderResult => ', orderResult)
+
+                console.log(debugTag, 'cancelling all orders')
+                const cancelResult = await privateConnector.deleteAllOrders({})
+                console.log(debugTag, 'cancelResult', cancelResult)
+            })
+    } catch (err) {
+        console.log(debugTag, 'websocket connection failed to connect', err)
+    }
+}
